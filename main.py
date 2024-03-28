93f7949dd475cc6bc1e5f283979460f3ef9c468e
rjniemela <rjniem@gmail.com>
2024-03-28 19:49:53 +0200

    Word and letter calculation routines

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_letter_count(input_text):
+    letters_dict = {}
+
+    for letter in input_text:
+        letter = letter.lower()
+
+        if letter not in letters_dict:
+            letters_dict[letter] = 0
+        else:
+            letters_dict[letter] = letters_dict[letter] + 1
+
+    return letters_dict
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letters_dict = get_letter_count(text)
     print(num_words)
+    print(letters_dict)
+
 
 main()
